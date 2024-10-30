e9f6b1f8b1164044a55928febc415fe97f4f2dcb
lisshat <imanim791@gmail.com>
2024-10-30 10:57:14 +0300

    Add root route and fix indentation

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 import os
 from app import create_app
+from flask import jsonify
 
 app = create_app()
+
+@app.route('/')  # Root path
+def home():
+    return jsonify({"message": "Welcome to the BasiGo API!"})
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
